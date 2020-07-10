8f46c46365e7ba67c25e706cc9082be2cf53a04b
Mohammadali Bahadoripoor <ali.bahadori@mail.de>
2020-07-10 12:40:47 +0200

    Initial Commit...

---
+++ MemeMe/MemeMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Ali Bahadori on 10.07.20.
+//  Copyright © 2020 Ali Bahadori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
